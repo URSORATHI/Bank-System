7dc010eca5376b671f558cb75b8960986a590e8c
ursorathi <sorathiunnati@gmail.com>
2021-06-15 12:57:39 +0530

    add files

---
+++ home/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from home.models import Customer
+from home.models import Transfer_Money
+# Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Transfer_Money)
+# Register your models here.
